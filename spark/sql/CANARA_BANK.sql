3c4ea243dd6d729c4b3952050ba7e308a24cee49
Dilip74832 <124902064+Dilip74832@users.noreply.github.com>
2023-06-09 17:14:04 +0530

    Update CANARA_BANK.sql

--- spark/sql/CANARA_BANK.sql
+++ spark/sql/CANARA_BANK.sql
@@ -1,14 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction`
---
 
 CREATE TABLE `transaction` (
   `sno` int(3) NOT NULL,
@@ -18,22 +11,14 @@ CREATE TABLE `transaction` (
   `datetime` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(3) NOT NULL,
   `name` text NOT NULL,
   `email` varchar(30) NOT NULL,
   `balance` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
+) 
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
 (1, 'Salman', 'khan@gmail.com', 50000),
@@ -47,35 +32,21 @@ INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
 (9, 'Nick', 'jonas@gmail.com', 30000),
 (10, 'Taapsee', 'pannu@gmail.com', 50000);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `transaction`
---
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`sno`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `transaction`
---
+
+
 ALTER TABLE `transaction`
   MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 COMMIT;
